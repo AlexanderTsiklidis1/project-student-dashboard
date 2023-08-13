8f1f7bb4dfe7cec3c345dd1985ff7bea1f82005d
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-08-13 02:17:09 -0400

    begins setup for app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
+import data from "./data/data.json";
+import component1 from "./components/Component1";
+import component2 from "./components/Component2";
+
+
 
 function App() {
-  return (
+  const [] = useState(data);
+  const [] = useState([""]);
+  const [] = useState([...students]);
+  const [] = useState("");
+    return (
     <div>
       <h1>Student Dashboard</h1>
     </div>
