46a31c886b63f2ee364b2d5dbb731303bc473365
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-08-13 06:10:16 -0400

    fixes total students bug

--- src/components/ListOfStudents.jsx
+++ src/components/ListOfStudents.jsx
@@ -5,7 +5,7 @@ export default function ListOfStudents({ currentCohort, studentsTitle}) {
         <div className="listOfStudents">
             <h1>{studentsTitle}</h1>
             <div className ="totalStudents">
-                Total Students: {currentCohort.Length}
+                Total Students: {currentCohort.length}
             </div>
             {currentCohort.map((student) => {
                 return <StudentComponent key={student.id} student={student} />
