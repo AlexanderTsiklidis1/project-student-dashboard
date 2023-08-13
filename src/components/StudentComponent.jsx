c37ccd465fa8ab9384557a4c2a47e369134b5de1
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-08-13 03:27:17 -0400

    attempts to build out show more... using processCertifi cation function

--- src/components/StudentComponent.jsx
+++ src/components/StudentComponent.jsx
@@ -25,24 +25,38 @@ export default function studentComponent({ student }) {
     const nameOfStudent = `${student.names.preferredName} ${student.names.middleName} 
     ${student.names.surName}`;
 
-    function getInfo(student) {
+    function showMore(student) {
         return (
             <>
-            <div>
-                <h3>CodeWars:</h3>
-                Current Total: {student.codewars.current.total} <br>
-                </br>
-                Last Week: {student.codewars.current.lastWeek} <br>
-                </br> 
-                Goal: {student.codewars.goal.total} <br>
-                </br>
-                Percent of Goal Acheived: {goalPercentage(student)}%
-            </div>
-            <div>
-                <h3>Scores</h3>
-                Assignments: {student.cohort.scores.assignments * 100}% <br>
-                </br>
-            </div>
+                <div>
+                    <h3>CodeWars:</h3>
+                    Current Total: {student.codewars.current.total} <br>
+                    </br>
+                    Last Week: {student.codewars.current.lastWeek} <br>
+                    </br> 
+                    Goal: {student.codewars.goal.total} <br>
+                    </br>
+                    Percent of Goal Acheived: {goalPercentage(student)}%
+                </div>
+                <div>
+                    <h3>Scores</h3>
+                    Assignments: {student.cohort.scores.assignments * 100}% <br>
+                    </br>
+                    Projects: {student.cohort.scores.projects * 100 }% <br>
+                    </br>
+                    Assessments: {student.cohort.scores.assessments * 100}% <br>
+                    </br>
+                </div>
+                <div>
+                    <h3>Certifications:</h3>
+                    Resume: {processCertification(student, "resume")} <br>
+                    </br>
+                    LinkedIn: {processCertification(student, "linkedin")} <br>
+                    </br> 
+                    Mock Interview: {processCertification(student, "mockInterview")} <br>
+                    </br>
+                    Github: {processCertification(student, "github")}%
+                </div>
             </>
         )
     }
