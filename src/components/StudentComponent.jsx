f37b2f160419b65cb8c64d9d8fd210865da64f64
Alexander Tsiklidis <alex@Alexanders-Air.lan>
2023-08-13 03:13:41 -0400

    builds skeleton functions for processing of certification and track of student

--- src/components/StudentComponent.jsx
+++ src/components/StudentComponent.jsx
@@ -2,6 +2,22 @@ import { useState } from "react";
 
 export default function studentComponent({ student }) {
     const [notes, setNotes] = useState(false);
-    
-    
+
+    function processCertification(student, certificationType) {
+        if (student.certifications[certificationType] == false) {
+            return "❌";
+        } else {
+            return "✅";
+        }
+    }
 }
+    function getTrack(student) {
+        if (student.codewars.current.total > 600
+                && student.certifications.resume && 
+                student.certifcations.linkedin && student.certifications.github && 
+                student.certifications.mockInterview == true 
+            ) 
+            {
+                return <>On Track to Graduate</>
+            }
+    }
